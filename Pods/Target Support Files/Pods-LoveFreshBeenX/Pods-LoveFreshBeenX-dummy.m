6b8a05c578ecf82d9012ff75d2a2aa28a34b733a
hsu <xuninghsu@gmail.com>
2016-06-09 14:59:30 +0800

    导入第三方框架

---
+++ Pods/Target Support Files/Pods-LoveFreshBeenX/Pods-LoveFreshBeenX-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LoveFreshBeenX : NSObject
+@end
+@implementation PodsDummy_Pods_LoveFreshBeenX
+@end
---
+++ Pods/Target Support Files/Pods-LoveFreshBeenX/Pods-LoveFreshBeenX-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_LoveFreshBeenXVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_LoveFreshBeenXVersionString[];
+
